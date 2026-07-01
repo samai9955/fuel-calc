c6f67f733813d0fa215f276eaba9ebed10b58d37
samai9955 <samai.saykhen@gmail.com>
2026-07-01 22:48:23 +0700

    Add proper PNG icons for PWA installability

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'fuelcalc-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon.svg'];
+const CACHE = 'fuelcalc-v2';
+const ASSETS = ['./index.html', './manifest.json', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
